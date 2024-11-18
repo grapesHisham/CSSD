d03184fd3be3c3abc6bd66db64a8815240f4d20e
Hishamm <HishamM@gmail.com>
2024-11-18 18:36:20 +0530

    request details

--- lib/app/modules/cssd_as_custodian/Department_User/view/widgets/dashboard_widgets/request_details_view.dart
+++ lib/app/modules/cssd_as_custodian/Department_User/view/widgets/dashboard_widgets/request_details_view.dart
@@ -1,8 +1,31 @@
+import 'package:cssd/app/modules/cssd_as_custodian/Department_User/controller/dashboard_controller_dept.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class RequestDetailsViewCssdCussDeptUser extends StatelessWidget {
+class RequestDetailsViewCssdCussDeptUser extends StatefulWidget {
+  const RequestDetailsViewCssdCussDeptUser({super.key});
+
+  @override
+  State<RequestDetailsViewCssdCussDeptUser> createState() =>
+      _RequestDetailsViewCssdCussDeptUserState();
+}
+
+class _RequestDetailsViewCssdCussDeptUserState
+    extends State<RequestDetailsViewCssdCussDeptUser> {
+  @override
+  void initState() {
+    super.initState();
+    final dashboardController =
+        Provider.of<DashboardControllerCssdCussDeptUser>(context,
+            listen: false);
+    dashboardController.fetchRequestDetails();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final dashboardController =
+        Provider.of<DashboardControllerCssdCussDeptUser>(context,
+            listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -10,8 +33,37 @@ class RequestDetailsViewCssdCussDeptUser extends StatelessWidget {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Container(
-        child: const Text("Request details"),
+      body: Column(
+        children: [
+          Expanded(
+            child: Consumer<DashboardControllerCssdCussDeptUser>(
+                builder: (context, dashboardConsumer, child) {
+              return ListView.builder(
+                itemCount: dashboardConsumer.requestDetailsList.length,
+                itemBuilder: (context, index) {
+                  final request = dashboardConsumer.requestDetailsList[index];
+                  return Card(
+                    child: ListTile(
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Request id: ${request.reqId}"),
+                          Text("Priority: ${request.priority}"),
+                          Text("Requested by : ${request.rUser}"),
+                          Text("Time : ${request.rTime}"),
+                          Text("Accepted  : ${request.isAccept == 1 ? 'Yes' : 'No'}"),
+                          request.isAccept == 1
+                              ? Text("Accepted by : ${request.acceptedUser}")
+                              : const Text(""),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            }),
+          )
+        ],
       ),
     );
   }
